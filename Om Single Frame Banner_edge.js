d877063cb3c85182bf3a8a1c8524dcfb58b39973
vulpino <kyle@vulpine.me>
2016-09-30 10:52:24 -0400

    om

---
+++ Om Single Frame Banner_edge.js
@@ -0,0 +1,71 @@
+
+(function(compId){var _=null,y=true,n=false,x22='151px',e34='${APP_Buttons_Violet_FS}',e56='${Mandala_092}',x2='5.0.0',x1='6.0.0',x4='rgba(0,0,0,0)',x54='Mandala_092',x53='335px',o='opacity',g='image',x39='OM_Logo_-Triskele_-_Corrected',x35='APP_Buttons_Violet_FS2',x44='Mandala_09',x50='100px',x49='885px',x30='0',e47='${Mandala_09}',x21='true',e43='${OM_Logo_-Triskele_-_Corrected}',x42='102px',x45='166px',m='rect',x20='0px',rz='rotateZ',x40='0.4',i='none',e52='${Repeatable_Clouds_x885}',x38='250px',x3='6.0.0.400',x25='-51px',x48='Repeatable_Clouds_x885',x23='34px',lf='left',x33='36px',x18='1',x14='56px',x29='APP_Buttons_Violet_FS',x6='rgba(9,0,0,1)',x28='60px',x13='252px',x16='0.6',x55='0.1',x27='256px',x15='auto',x26='-12px',x37='-74px',x11='-50px',e24='${APP_Button_-_Violet_Fill}',x10='rgba(255,255,255,1)',tp='top',e36='${APP_Buttons_Violet_FS2}',x17='APP_Button_-_Violet_Fill',x32='154px',x12='-11px';var g9='APP%20Buttons%20Violet%20WS.png',g19='APP%20Button%20-%20Violet%20Fill.png',g51='Repeatable%20Clouds%20x885.jpg',g46='Mandala_09.png',g7='OmText-03.png',g41='OM%20Logo%20-Triskele%20-%20Corrected.png',g31='APP%20Buttons%20Violet%20FS.png',g5='Mountain%20Overlay%20Final.png',g8='Begin%20Your%20Journey2.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={'acme, sans-serif':'<script src=\"http://use.edgefonts.net/acme:n4:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'TheClouds',symbolName:'TheClouds',t:m,r:['0','0','885','100','auto','auto']},{id:'Mountain_Overlay_Final',t:g,r:['0px','0','335px','280px','auto','auto'],f:[x4,im+g5,'0px','0px']},{id:'Mandala',symbolName:'Mandala',t:m,r:['84','27','166','166','auto','auto']},{id:'DropShadow',t:m,r:['125px','189px','80px','36px','auto','auto'],f:[x6],s:[0,"rgb(0, 0, 0)",i],filter:[0,0,1,1,0,0,0,16.438356164384,"rgba(0,0,0,0)",0,0,0]},{id:'OmText-03',t:g,r:['134px','187px','67px','36px','auto','auto'],f:[x4,im+g7,'0px','0px']},{id:'OM_Logo',symbolName:'OM_Logo',t:m,r:['116','57','102','102','auto','auto']},{id:'Rectangle2',t:m,r:['13px','7px','309px','14px','auto','auto'],f:[x6],s:[0,"rgb(0, 0, 0)",i],filter:[0,0,1,1,0,0,0,8.2191780821918,"rgba(0,0,0,0)",0,0,0]},{id:'Begin_Your_Journey2',t:g,r:['13px','7px','309px','14px','auto','auto'],f:[x4,im+g8,'0px','0px']},{id:'APP_Buttons_Violet_WS',t:g,r:['39px','219px','256px','60px','auto','auto'],f:[x4,im+g9,'0px','0px'],tf:[[],[],[],['0.6','0.6']]},{id:'getbutton01',symbolName:'getbutton01',t:m,r:['90','231','154','36','auto','auto'],cu:'pointer'}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'335px','280px'],overflow:'hidden',f:[x10]}}},tt:{d:30124,a:y,data:[]}},"APP_Button_Fill":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x11,x12,x13,x14,x15,x15],tf:[[],[],[],[x16,x16]],id:x17,o:x18,t:g,f:[x4,im+g19,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x22,x23]}}},tt:{d:1500,a:n,data:[["eid62",o,0,1500,"linear",e24,'1','0']]}},"getbuttonviolet":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x25,x26,x27,x28,x15,x15],tf:[[],[],[],[x16,x16]],id:x29,o:x30,t:g,f:[x4,im+g31,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x32,x33]}}},tt:{d:500,a:y,data:[["eid64",o,0,500,"linear",e34,'1','0']]}},"getbutton01":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x25,x26,x27,x28,x15,x15],tf:[[],[],[],[x16,x16]],id:x35,o:x30,t:g,f:[x4,im+g31,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x32,x33]}}},tt:{d:500,a:n,data:[["eid66",o,0,500,"linear",e36,'1','0']]}},"OM_Logo":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x37,x37,x38,x38,x15,x15],id:x39,tf:[[],[],[],[x40,x40]],t:g,f:[x4,im+g41,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x42,x42]}}},tt:{d:30124,a:y,data:[["eid52",rz,0,30124,"linear",e43,'359deg','0deg']]}},"Mandala":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x44,t:g,r:[x20,x20,x45,x45,x15,x15],f:[x4,im+g46,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x45,x45]}}},tt:{d:30124,a:y,data:[["eid12",rz,0,30124,"linear",e47,'0deg','359deg']]}},"TheClouds":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x48,t:g,r:[x20,x20,x49,x50,x15,x15],f:[x4,im+g51,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x49,x50]}}},tt:{d:30124,a:y,data:[["eid9",lf,0,30124,"linear",e52,'0px','-550px'],["eid2",tp,0,0,"linear",e52,'0px','0px'],["eid8",tp,30124,0,"linear",e52,'0px','0px']]}},"AnotherMandala":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x20,x20,x53,x53,x15,x15],id:x54,o:x55,t:g,f:[x4,im+g46,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x53,x53]}}},tt:{d:10000,a:y,data:[["eid70",rz,0,10000,"linear",e56,'0deg','359deg']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-675169535");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'APP_Button_Fill'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${APP_Button_-_Violet_Fill}","click",function(sym,e){window.open("http://om0016.businesscatalyst.com","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${APP_Button_-_Violet_Fill}","mouseover",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${APP_Button_-_Violet_Fill}","mouseout",function(sym,e){});
+//Edge binding end
+})("APP_Button_Fill");
+//Edge symbol end:'APP_Button_Fill'
+
+//=========================================================
+
+//Edge symbol: 'getbuttonviolet'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${APP_Buttons_Violet_FS}","mouseover",function(sym,e){});
+//Edge binding end
+})("getbuttonviolet");
+//Edge symbol end:'getbuttonviolet'
+
+//=========================================================
+
+//Edge symbol: 'getbutton01'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${APP_Buttons_Violet_FS2}","mouseover",function(sym,e){sym.play();sym.$("AnotherMandala").animate({opacity:0.1},1);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${APP_Buttons_Violet_FS2}","mouseout",function(sym,e){sym.playReverse();sym.$("AnotherMandala").animate({opacity:0},1);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${APP_Buttons_Violet_FS2}","click",function(sym,e){window.open("http://om0016.businesscatalyst.com","_blank");});
+//Edge binding end
+})("getbutton01");
+//Edge symbol end:'getbutton01'
+
+//=========================================================
+
+//Edge symbol: 'OM_Logo'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30124,function(sym,e){this.play(0);});
+//Edge binding end
+})("OM_Logo");
+//Edge symbol end:'OM_Logo'
+
+//=========================================================
+
+//Edge symbol: 'Mandala'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30124,function(sym,e){this.play(0);});
+//Edge binding end
+})("Mandala");
+//Edge symbol end:'Mandala'
+
+//=========================================================
+
+//Edge symbol: 'TheClouds'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",30124,function(sym,e){this.play(0);});
+//Edge binding end
+})("TheClouds");
+//Edge symbol end:'TheClouds'
+
+//=========================================================
+
+//Edge symbol: 'AnotherMandala'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10000,function(sym,e){this.play(0);});
+//Edge binding end
+})("AnotherMandala");
+//Edge symbol end:'AnotherMandala'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-675169535");
